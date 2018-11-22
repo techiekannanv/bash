8f202d496e3dd63a9879126f0501c4ac9b332b9b
techiekannanv <techiekannanv@gmail.com>
2018-11-22 21:55:50 +0530

    Update passwdgen.py

--- passwdgen.py
+++ passwdgen.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """This is just simple password generator using random module.
 This will create 8 char password with combination of alpha,
 numeric and special character. The below condition should met
@@ -13,12 +14,11 @@ import random
 
 def gen(length=8):
     chars = dict(special=0, lower=0, upper=0, numeric=0)
-    all_chars = string.printable.replace(string.whitespace, '')
+    all_chars = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
     passwd = ''
-    tried = 1
     while len(passwd) != length:
         char = random.choice(all_chars)
-        if char not in passwd:
+        if char not in passwd and char != '\\':
             if char in string.punctuation:
                 chars['special'] = 1
             elif char in string.ascii_lowercase:
@@ -30,10 +30,8 @@ def gen(length=8):
             passwd += char
             all_chars = all_chars.replace(char, '')
             if len(passwd) == length and list(chars.values()) != [1, 1, 1, 1]:
-                tried += 1
                 passwd = ''
-                all_chars = string.printable.replace(string.whitespace, '')
-    # print('DEBUG: Total tried ',tried,' times')
+                all_chars = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
     return passwd
 
 
